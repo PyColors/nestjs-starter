ed3f5de86d612b6b0876b56b35a601938a6b3902
PyColors <patweb44@gmail.com>
2022-05-02 21:38:56 +0200

    Adding relationships with Postgres and TypeORM

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,22 +1,30 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+  Req,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+} from '@nestjs/common';
 import PostsService from './posts.service';
 import CreatePostDto from './dto/createPost.dto';
 import UpdatePostDto from './dto/updatePost.dto';
 import JwtAuthenticationGuard from '../authentication/jwt-authentication.guard';
 import FindOneParams from '../utils/findOneParams';
+import RequestWithUser from '../authentication/requestWithUser.interface';
 
 @Controller('posts')
+@UseInterceptors(ClassSerializerInterceptor)
 export default class PostsController {
   constructor(
     private readonly postsService: PostsService
   ) {}
 
-  @Post()
-  @UseGuards(JwtAuthenticationGuard)
-  async createPost(@Body() post: CreatePostDto) {
-    return this.postsService.createPost(post);
-  }
-  
   @Get()
   getAllPosts() {
     return this.postsService.getAllPosts();
@@ -25,6 +33,12 @@ export default class PostsController {
   @Get(':id')
   getPostById(@Param() { id }: FindOneParams) {
     return this.postsService.getPostById(Number(id));
+  }
+
+  @Post()
+  @UseGuards(JwtAuthenticationGuard)
+  async createPost(@Body() post: CreatePostDto, @Req() req: RequestWithUser) {
+    return this.postsService.createPost(post, req.user);
   }
 
   @Patch(':id')
